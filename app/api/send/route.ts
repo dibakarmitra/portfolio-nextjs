40a401ab508113dbbb8884435e53820191a4161a
Dibakar Mitra <dibakar.s@avantaifintech.com>
2024-12-04 23:03:48 +0530

    contact form functions

---
+++ app/api/send/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+import { contactConfig } from "@/config/contact";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request: Request) {
+  try {
+    const { name, email, message } = await request.json();
+
+    const data = await resend.emails.send({
+      from: contactConfig.email.from,
+      to: contactConfig.email.to,
+      subject: contactConfig.email.subject(name),
+      reply_to: email,
+      text: contactConfig.email.emailTemplate.text({ name, email, message }),
+      html: contactConfig.email.emailTemplate.html({ name, email, message }),
+    });
+
+    return NextResponse.json({ success: true, data });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: (error as Error).message,
+    });
+  }
+}
