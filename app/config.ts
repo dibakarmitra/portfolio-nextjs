a5148790a5e9544b59267100f9582c0ad2d9eeba
Dibakar Mitra <dibakar.s@avantaifintech.com>
2024-12-06 17:40:48 +0530

    issue fix

---
+++ app/config.ts
@@ -0,0 +1,12 @@
+export const metaData = {
+  title: 'Dibakar Mitra',
+  author: 'Dibakar Mitra',
+  description: 'Personal portfolio and blog',
+  language: 'en-us',
+  siteUrl: 'https://dibakarmitra.vercel.app',
+  siteRepo: 'https://github.com/dibakarmitra/portfolio-nextjs',
+  email: 'hello@dibakarmitra.com',
+  github: 'https://github.com/dibakarmitra',
+  twitter: 'https://twitter.com/dibakarmitra',
+  linkedin: 'https://linkedin.com/in/dibakarmitra',
+};
